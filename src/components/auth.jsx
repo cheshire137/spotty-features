2f8366fc9375fbca2840b0e8d014e461e2f3d6f2
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 13:47:43 -0600

    Add Auth component

---
+++ src/components/auth.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Auth extends React.Component {
+  render() {
+    return <a href="https://accounts.spotify.com/authorize">Sign into Spotify</a>;
+  }
+}
