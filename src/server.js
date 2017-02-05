cb815d7c00cc5610a50df104c1c576c03a4b8b86
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 14:54:07 -0600

    Make all URLs render index.html
    So React can handle the routing.

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.listen(3000, function() {
   console.log('spotty-features listening on port 3000')
 })
