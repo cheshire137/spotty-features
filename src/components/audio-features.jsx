27836a4f03467097dae3802dd0543fe31466c45c
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 17:45:46 -0600

    Color code features to indicate prominence

--- src/components/audio-features.jsx
+++ src/components/audio-features.jsx
@@ -1,19 +1,52 @@
 import React from 'react'
 
 class AudioFeatures extends React.Component {
+  percent(number) {
+    const percentage = Math.round(number * 100)
+    return `${percentage}%`
+  }
+
+  classForScale(number) {
+    if (number <= 0.25) {
+      return 'audio-feature-25'
+    }
+    if (number <= 0.5) {
+      return 'audio-feature-50'
+    }
+    if (number <= 0.75) {
+      return 'audio-feature-75'
+    }
+    return 'audio-feature-100'
+  }
+
   render() {
     const { acousticness, danceability, energy, instrumentalness, liveness,
             loudness, speechiness, valence } = this.props
+    const acousticClass = this.classForScale(acousticness)
+    const danceClass = this.classForScale(danceability)
+    const energyClass = this.classForScale(energy)
+    const instClass = this.classForScale(instrumentalness)
+    const liveClass = this.classForScale(liveness)
+    const speechClass = this.classForScale(speechiness)
+    const valClass = this.classForScale(valence)
     return (
-      <dl>
-        <dt>Acoustic:</dt><dd>{acousticness}</dd>
-        <dt>Danceable:</dt><dd>{danceability}</dd>
-        <dt>Energy:</dt><dd>{energy}</dd>
-        <dt>Instrumental:</dt><dd>{instrumentalness}</dd>
-        <dt>Live:</dt><dd>{liveness}</dd>
-        <dt>Loud:</dt><dd>{loudness}</dd>
-        <dt>Speech:</dt><dd>{speechiness}</dd>
-        <dt>Valence:</dt><dd>{valence}</dd>
+      <dl className="audio-features">
+        <dt className={acousticClass}>Acoustic:</dt>
+        <dd className={acousticClass}>{this.percent(acousticness)}</dd>
+        <dt className={danceClass}>Danceable:</dt>
+        <dd className={danceClass}>{this.percent(danceability)}</dd>
+        <dt className={energyClass}>Energy:</dt>
+        <dd className={energyClass}>{this.percent(energy)}</dd>
+        <dt className={instClass}>Instrumental:</dt>
+        <dd className={instClass}>{this.percent(instrumentalness)}</dd>
+        <dt className={liveClass}>Live:</dt>
+        <dd className={liveClass}>{this.percent(liveness)}</dd>
+        <dt>Loud:</dt>
+        <dd>{loudness} dB</dd>
+        <dt className={speechClass}>Speech:</dt>
+        <dd className={speechClass}>{this.percent(speechiness)}</dd>
+        <dt className={valClass}>Valence:</dt>
+        <dd className={valClass}>{this.percent(valence)}</dd>
       </dl>
     )
   }
