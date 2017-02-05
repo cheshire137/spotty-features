ac9af712bc0ea078d31cc136710f2726c76daa00
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 17:28:13 -0600

    List audio features of each track

--- src/components/audio-features.jsx
+++ src/components/audio-features.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 class AudioFeatures extends React.Component {
   render() {
+    const { acousticness, danceability, energy, instrumentalness, liveness,
+            loudness, speechiness, valence } = this.props
     return (
-      <p>hey</p>
+      <dl>
+        <dt>Acoustic:</dt><dd>{acousticness}</dd>
+        <dt>Danceable:</dt><dd>{danceability}</dd>
+        <dt>Energy:</dt><dd>{energy}</dd>
+        <dt>Instrumental:</dt><dd>{instrumentalness}</dd>
+        <dt>Live:</dt><dd>{liveness}</dd>
+        <dt>Loud:</dt><dd>{loudness}</dd>
+        <dt>Speech:</dt><dd>{speechiness}</dd>
+        <dt>Valence:</dt><dd>{valence}</dd>
+      </dl>
     )
   }
 }
