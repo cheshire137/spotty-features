afad0a2adbc1103a1e984a2d9c97faaecd6d83fd
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 14:59:59 -0600

    Add LocalStorage model

---
+++ src/models/local-storage.js
@@ -0,0 +1,29 @@
+const localStorageKey = 'spotty-features'
+
+export default class LocalStorage {
+  static getJSON() {
+    if (!window.localStorage) {
+      console.error('browser does not support local storage');
+      return {}
+    }
+    const appData = window.localStorage.getItem(localStorageKey) || '{}'
+    return JSON.parse(appData)
+  }
+
+  static get(key) {
+    const appData = this.getJSON()
+    return appData[key]
+  }
+
+  static set(key, value) {
+    const appData = this.getJSON()
+    appData[key] = value
+    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+  }
+
+  static delete(key) {
+    const appData = this.getJSON()
+    delete appData[key]
+    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+  }
+}
