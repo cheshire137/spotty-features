94c7af6e4ebec4e8dc8cd7d5274bedc8ea8757fd
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 13:56:36 -0600

    Fix how auth is required and used

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Auth from './components/auth'
+import Auth from './components/auth.jsx'
 
-ReactDOM.render(Auth, document.getElementById('root'))
+ReactDOM.render(<Auth />, document.getElementById('root'))
