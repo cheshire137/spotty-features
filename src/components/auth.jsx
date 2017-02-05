1a2dc9fbea6e58bae02a5ecd6a300cedc835e7f1
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 14:22:27 -0600

    Make use of client ID to build auth URL

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Config from '../public/config.json'
 
 export default class Auth extends React.Component {
   render() {
-    return <a href="https://accounts.spotify.com/authorize">Sign into Spotify</a>;
+    const host = 'https://accounts.spotify.com'
+    const redirectUri = `${window.location.protocol}//${window.location.host}/`
+    const authUrl = `${host}/authorize?response_type=code` +
+      `&redirect_uri=${redirectUri}&client_id=${Config.spotify.clientId}`
+    return <a href={authUrl}>Sign into Spotify</a>;
   }
 }
