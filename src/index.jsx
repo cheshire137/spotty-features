1368b1bf75d989bad96cd9d8c7edbac0e488f10f
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 15:30:56 -0600

    Ensure we have Promise

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom'
 import routes from './routes.jsx'
 
+import Promise from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = Promise
+}
+
 ReactDOM.render(routes, document.getElementById('root'))
