aed5a2f5e76a18bb8be959f5471469339246ec20
Sarah Vessels <cheshire137@gmail.com>
2017-02-05 21:05:34 -0600

    Use PORT env var in server if set

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
+app.set('port', (process.env.PORT || 3000))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
@@ -13,6 +15,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(3000, function() {
-  console.log('spotty-features listening on port 3000')
+app.listen(app.get('port'), function() {
+  console.log(`spotty-features listening on port ${app.get('port')}`)
 })
