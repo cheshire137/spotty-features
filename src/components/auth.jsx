be2b3a7d522d1d2cc1aaaad5614ea9fe1b6ee994
Sarah Vessels <cheshire137@gmail.com>
2017-02-06 12:13:42 -0600

    Use Spotify green on button

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -18,7 +18,10 @@ export default class Auth extends React.Component {
               Sign in with your Spotify account to see trends about your saved songs.
             </h2>
             <p>
-              <a href={authUrl} className="button is-large is-primary">Sign into Spotify</a>
+              <a
+                href={authUrl}
+                className="spotify-button is-primary button is-large"
+              >Sign into Spotify</a>
             </p>
           </div>
         </div>
