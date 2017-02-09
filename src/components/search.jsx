ad698aab787396834f2fae1a955687952b21ed54
Sarah Vessels <cheshire137@gmail.com>
2017-02-08 21:13:11 -0600

    Add buttons to go back, list recommendations

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,6 +4,7 @@ import { debounce } from 'throttle-debounce'
 import ArtistSeedSummary from './artist-seed-summary.jsx'
 import SearchResultArtist from './search-result-artist.jsx'
 import SearchResultTrack from './search-result-track.jsx'
+import TrackRecommendation from './track-recommendation.jsx'
 import TrackSeedSummary from './track-seed-summary.jsx'
 
 import Features from '../models/features.js'
@@ -16,6 +17,7 @@ class Search extends React.Component {
     this.state = {
       seedType: 'track',
       results: [],
+      recommendations: [],
       seedQuery: '',
       acousticness: 0.5,
       danceability: 0.5,
@@ -48,7 +50,23 @@ class Search extends React.Component {
   }
 
   onRecommendations(json) {
-    console.log(json)
+    const recommendations = []
+    for (const item of json.tracks) {
+      const images = item.album.images.filter(img => img.width < 100)
+      const track = {
+        id: item.id,
+        name: item.name,
+        artists: item.artists.map(artist => artist.name),
+        album: item.album.name,
+        url: item.external_urls.spotify,
+        albumUrl: item.album.external_urls.spotify
+      }
+      if (images.length > 0) {
+        track.image = images[0].url
+      }
+      recommendations.push(track)
+    }
+    this.setState({ recommendations })
   }
 
   onRecommendationsError(error) {
@@ -62,7 +80,12 @@ class Search extends React.Component {
   }
 
   onSeedTypeChange(event) {
-    this.setState({ seedType: event.target.value, seed: null, seedQuery: '' })
+    this.setState({
+      seedType: event.target.value,
+      seed: null,
+      seedQuery: '',
+      recommendations: []
+    })
   }
 
   onSeedQueryChange(event) {
@@ -214,49 +237,85 @@ class Search extends React.Component {
     )
   }
 
+  changeSeed() {
+    this.setState({ seed: null, results: [] })
+  }
+
   recommendationsForm() {
-    const { seed } = this.state
-    if (!seed) {
+    const { seed, recommendations } = this.state
+    if (!seed || recommendations.length > 0) {
       return
     }
     return (
-      <form onSubmit={e => this.onRecommendationsSubmit(e)}>
-        <h4 className="refine-title title is-4">
-          Step 2: Refine your results
-        </h4>
-        {Features.fields.map(feature => {
-          return (
-            <div key={feature} className="control is-horizontal">
-              <div className="control-label">
-                <label className="label" htmlFor={feature}>
-                  {Features.labels[feature]}
-                </label>
+      <div>
+        <button
+          type="button"
+          className="button is-small"
+          onClick={() => this.changeSeed()}
+        >&larr; Change seed</button>
+        <form onSubmit={e => this.onRecommendationsSubmit(e)}>
+          <h4 className="refine-title title is-4">
+            Step 2: Refine your results
+          </h4>
+          {Features.fields.map(feature => {
+            return (
+              <div key={feature} className="control is-horizontal">
+                <div className="control-label">
+                  <label className="label" htmlFor={feature}>
+                    {Features.labels[feature]}
+                  </label>
+                </div>
+                <div className="control">
+                  <span className="feature-range-min">0%</span>
+                  <input
+                    onChange={e => this.onFeatureChange(e, feature)}
+                    id={feature}
+                    value={this.state[feature]}
+                    type="range"
+                    min="0"
+                    max="1"
+                    step="0.05"
+                    className="slider"
+                  />
+                  <span className="feature-range-max">100%</span>
+                  <span className="feature-percentage">{Math.round(this.state[feature] * 100)}%</span>
+                </div>
               </div>
-              <div className="control">
-                <span className="feature-range-min">0%</span>
-                <input
-                  onChange={e => this.onFeatureChange(e, feature)}
-                  id={feature}
-                  value={this.state[feature]}
-                  type="range"
-                  min="0"
-                  max="1"
-                  step="0.05"
-                  className="slider"
-                />
-                <span className="feature-range-max">100%</span>
-                <span className="feature-percentage">{Math.round(this.state[feature] * 100)}%</span>
-              </div>
-            </div>
-          )
-        })}
-        <div className="control">
-          <button
-            type="submit"
-            className="button is-primary is-large"
-          >Find Songs</button>
-        </div>
-      </form>
+            )
+          })}
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary is-large"
+            >Find Songs</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  changeAudioFeatures() {
+    this.setState({ recommendations: [] })
+  }
+
+  recommendationsList() {
+    const { recommendations } = this.state
+    if (recommendations.length < 1) {
+      return
+    }
+    return (
+      <div>
+        <button
+          type="button"
+          className="button is-small"
+          onClick={() => this.changeAudioFeatures()}
+        >&larr; Change filters</button>
+        <ul className="recommendations-list">
+          {recommendations.map(track => {
+            return <TrackRecommendation key={track.id} {...track} />
+          })}
+        </ul>
+      </div>
     )
   }
 
@@ -266,6 +325,7 @@ class Search extends React.Component {
         {this.seedSearchForm()}
         {this.seedSummary()}
         {this.recommendationsForm()}
+        {this.recommendationsList()}
       </div>
     )
   }
