fafbc08ba30adc8a092fb1719d192fd818353ead
Sarah Vessels <cheshire137@gmail.com>
2017-02-18 13:14:45 -0600

    Add test for recommendations request

--- test/models/spotify-api.test.js
+++ test/models/spotify-api.test.js
@@ -2,6 +2,7 @@ import fetchMock from 'fetch-mock'
 
 import Config from '../../src/public/config'
 import MeResponse from '../fixtures/spotify/me'
+import RecommendationsResponse from '../fixtures/spotify/recommendations'
 import SpotifyApi from '../../src/models/spotify-api'
 import TrackSearchResponse from '../fixtures/spotify/track-search'
 
@@ -33,6 +34,30 @@ describe('SpotifyApi', () => {
     const api = new SpotifyApi('123abc')
     return api.search(opts).then(data => {
       expect(data).toEqual(TrackSearchResponse)
+    })
+  })
+
+  test('gets track recommendations', () => {
+    const path = 'recommendations?limit=1&seed_tracks=6DLJQz0hav4l4VfEjJze5T' +
+      '&target_acousticness=0.26&target_danceability=0.595&target_energy=0.649' +
+      '&target_valence=0.376&target_instrumentalness=0.00143' +
+      '&target_liveness=0.166&target_speechiness=0.0247'
+    fetchMock.get(`${Config.spotify.apiUrl}/${path}`, RecommendationsResponse)
+
+    const opts = {
+      limit: 1,
+      seed_tracks: '6DLJQz0hav4l4VfEjJze5T',
+      target_acousticness: 0.26,
+      target_danceability: 0.595,
+      target_energy: 0.649,
+      target_valence: 0.376,
+      target_instrumentalness: 0.00143,
+      target_liveness: 0.166,
+      target_speechiness: 0.0247
+    }
+    const api = new SpotifyApi('123abc')
+    return api.getRecommendations(opts).then(data => {
+      expect(data).toEqual(RecommendationsResponse)
     })
   })
 })
