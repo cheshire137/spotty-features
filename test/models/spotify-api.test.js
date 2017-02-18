9c80b2f9c99f8970bd3c3c372b741b46a09e26c2
Sarah Vessels <cheshire137@gmail.com>
2017-02-18 12:59:52 -0600

    Update test to mock exact URL being hit

--- test/models/spotify-api.test.js
+++ test/models/spotify-api.test.js
@@ -1,8 +1,8 @@
 import fetchMock from 'fetch-mock'
 
-import SpotifyApi from '../../src/models/spotify-api'
-
+import Config from '../../src/public/config'
 import MeResponse from '../fixtures/spotify/me'
+import SpotifyApi from '../../src/models/spotify-api'
 
 describe('SpotifyApi', () => {
   test('uses given token in auth header', () => {
@@ -11,7 +11,7 @@ describe('SpotifyApi', () => {
   })
 
   test('gets info about the user', () => {
-    fetchMock.get('*', MeResponse)
+    fetchMock.get(`${Config.apiUrl}/me`, MeResponse)
 
     const api = new SpotifyApi('123abc')
     return api.me().then(data => {
