75912ce1ffc30e9dd34f24770c90a15658930cbd
Sarah Vessels <cheshire137@gmail.com>
2017-02-18 12:56:46 -0600

    Test me() in api

--- test/models/spotify-api.test.js
+++ test/models/spotify-api.test.js
@@ -1,6 +1,21 @@
+import fetchMock from 'fetch-mock'
+
 import SpotifyApi from '../../src/models/spotify-api'
 
-test('uses given token in auth header', () => {
-  const api = new SpotifyApi('123abc')
-  expect(api.headers.Authorization).toBe('Bearer 123abc')
+import MeResponse from '../fixtures/spotify/me'
+
+describe('SpotifyApi', () => {
+  test('uses given token in auth header', () => {
+    const api = new SpotifyApi('123abc')
+    expect(api.headers.Authorization).toBe('Bearer 123abc')
+  })
+
+  test('gets info about the user', () => {
+    fetchMock.get('*', MeResponse)
+
+    const api = new SpotifyApi('123abc')
+    return api.me().then(data => {
+      expect(data).toEqual(MeResponse)
+    })
+  })
 })
