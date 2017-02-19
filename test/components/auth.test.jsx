34c28f8f724a6a7d1975bf160bc11859818ac16a
Sarah Vessels <cheshire137@gmail.com>
2017-02-19 17:07:20 -0600

    Add test for client ID in auth URL

--- test/components/auth.test.jsx
+++ test/components/auth.test.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import Auth from '../../src/components/auth.jsx'
+import Config from '../../src/public/config'
 
 describe('Auth', () => {
+  let component = null
+
+  beforeEach(() => {
+    component = <Auth />
+  })
+
   test('matches snapshot', () => {
-    const component = <Auth />
     const tree = renderer.create(component).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  test('includes Spotify client ID in link', () => {
+    const link = shallow(component).find('.spotify-button')
+    const regex = new RegExp(Config.spotify.clientId)
+    expect(link.props().href).toMatch(regex)
   })
 })
