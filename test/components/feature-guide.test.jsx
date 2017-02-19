16067985a210cb5f95fda872ec620b7a40ffdd8a
Sarah Vessels <cheshire137@gmail.com>
2017-02-18 21:29:33 -0600

    Add FeatureGuide test for activeView

--- test/components/feature-guide.test.jsx
+++ test/components/feature-guide.test.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 
 import FeatureGuide from '../../src/components/feature-guide.jsx'
 
 describe('FeatureGuide', () => {
-  let component = null
-
-  beforeEach(() => {
-    component = <FeatureGuide activeView="trends" />
-  })
-
   test('matches snapshot', () => {
+    const component = <FeatureGuide activeView="trends" />
     const tree = renderer.create(component).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  test('changes class based on active view', () => {
+    let component = <FeatureGuide activeView="search" />
+    expect(shallow(component).find('.title.search-view').length).toBe(1)
+
+    component = <FeatureGuide activeView="trends" />
+    expect(shallow(component).find('.title.search-view').length).toBe(0)
   })
 })
