f3f5e22d85ea1f4415547c51ccf9c86ddac34146
Sarah Vessels <cheshire137@gmail.com>
2017-02-20 10:53:37 -0600

    Add test for expired token in Spotify component

--- test/components/spotify.test.jsx
+++ test/components/spotify.test.jsx
@@ -2,7 +2,7 @@ import fetchMock from 'fetch-mock'
 import MockDate from 'mockdate'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import Config from '../../src/public/config'
 import Spotify from '../../src/components/spotify.jsx'
@@ -10,20 +10,26 @@ import Spotify from '../../src/components/spotify.jsx'
 import mockLocalStorage from '../mocks/local-storage'
 import waitForRequests from '../helpers/wait-for-requests'
 
+import MeResponse from '../fixtures/spotify/me'
 import MultiAudioFeaturesResponse from '../fixtures/spotify/multi-audio-features'
 import SavedTracksResponse from '../fixtures/spotify/saved-tracks'
+import UnauthorizedResponse from '../fixtures/spotify/unauthorized'
 
-const initialLocalData = { 'spotify-token': '123abc' }
+const initialLocalData = {
+  'spotify-token': '123abc' ,
+  'spotify-user': MeResponse.display_name,
+  'spotify-avatar-url': MeResponse.images[0].url
+}
 
 describe('Spotify', () => {
+  const tracksPath = 'me/tracks?limit=50&offset=0'
   let component = null
-  // let path = null
+  let path = null
   let store = null
-  let tracksReq = null
   let featuresReq = null
 
-  const routeChange = () => {
-    // path = newPath
+  const routeChange = newPath => {
+    path = newPath
   }
 
   beforeEach(() => {
@@ -32,12 +38,8 @@ describe('Spotify', () => {
     store = { 'spotty-features': JSON.stringify(initialLocalData) }
     mockLocalStorage(store)
 
-    const path1 = 'me/tracks?limit=50&offset=0'
-    tracksReq = fetchMock.get(`${Config.spotify.apiUrl}/${path1}`,
-                              SavedTracksResponse)
-
-    const path2 = 'audio-features?ids=4nDfc6F2uVcc6wdG7kBzWO'
-    featuresReq = fetchMock.get(`${Config.spotify.apiUrl}/${path2}`,
+    const path = 'audio-features?ids=4nDfc6F2uVcc6wdG7kBzWO'
+    featuresReq = fetchMock.get(`${Config.spotify.apiUrl}/${path}`,
                                 MultiAudioFeaturesResponse)
 
     component = <Spotify numWeeks={1} router={{ push: routeChange }} />
@@ -46,16 +48,33 @@ describe('Spotify', () => {
   afterEach(fetchMock.restore)
 
   test('matches snapshot', () => {
+    fetchMock.get(`${Config.spotify.apiUrl}/${tracksPath}`, SavedTracksResponse)
+
     const tree = renderer.create(component).toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   test('shows recently saved tracks', done => {
+    const tracksReq = fetchMock.get(`${Config.spotify.apiUrl}/${tracksPath}`,
+                                    SavedTracksResponse)
+
     const wrapper = mount(component)
     expect(wrapper.find('.week-list-container').length).toBe(0)
 
     waitForRequests([tracksReq, featuresReq], done, () => {
       expect(wrapper.find('.week-list-container').length).toBe(1)
+    })
+  })
+
+  test('handles expired token when fetching tracks', done => {
+    const resp = { body: UnauthorizedResponse, status: 401 }
+    const tracksReq = fetchMock.get(`${Config.spotify.apiUrl}/${tracksPath}`, resp)
+
+    mount(component)
+
+    waitForRequests([tracksReq], done, () => {
+      expect(path).toBe('/')
+      expect(store).toEqual({ 'spotty-features': '{}' })
     })
   })
 })
