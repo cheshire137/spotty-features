7addf9ea1d8b98d3d95d5e75393c39f0889cb0eb
Sarah Vessels <cheshire137@gmail.com>
2017-02-20 14:52:48 -0600

    Add snapshot test for RecommendationsList

---
+++ test/components/recommendations-list.test.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+
+import RecommendationsList from '../../src/components/recommendations-list.jsx'
+
+import RecommendationsResponse from '../fixtures/spotify/recommendations'
+import TrackSearchResponse from '../fixtures/spotify/track-search'
+
+const recommendation = RecommendationsResponse.tracks[0]
+
+const track = TrackSearchResponse.tracks.items[0]
+const trackSeed = {
+  id: track.id,
+  name: track.name,
+  url: track.external_urls.spotify,
+  artists: track.artists.map(artist => artist.name),
+  album: track.album.name,
+  image: track.album.images[2].url,
+  albumUrl: track.album.external_urls.spotify
+}
+
+function props(opts) {
+  return {
+    unauthorized: opts.unauthorized,
+    changeAudioFeatures: opts.changeAudioFeatures,
+    recommendations: [
+      {
+        album: recommendation.album.name,
+        albumUrl: recommendation.album.external_urls.spotify,
+        artists: recommendation.artists.map(a => a.name),
+        id: recommendation.id,
+        image: recommendation.album.images[2].url,
+        name: recommendation.name,
+        spotifyUri: recommendation.uri,
+        url: recommendation.external_urls.spotify
+      }
+    ],
+    token: '123abc',
+    seedType: 'track',
+    seed: trackSeed
+  }
+}
+
+describe('RecommendationsList', () => {
+  let component = null
+  let wasUnauthorized = false
+  let wantToChangeAudioFeatures = false
+
+  const changeAudioFeatures = () => {
+    wantToChangeAudioFeatures = true
+  }
+
+  const unauthorized = () => {
+    wasUnauthorized = true
+  }
+
+  beforeEach(() => {
+    const opts = {
+      changeAudioFeatures, unauthorized
+    }
+    component = <RecommendationsList {...props(opts)} />
+  })
+
+  afterEach(() => {
+    wantToChangeAudioFeatures = false
+    wasUnauthorized = false
+  })
+
+  test('matches snapshot', () => {
+    const tree = renderer.create(component).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
