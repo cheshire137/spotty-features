a355743ea7806552418df36465e3047b82bf5edc
Sarah Vessels <cheshire137@gmail.com>
2017-02-20 15:07:43 -0600

    Add tests for track count, playlist form in rec list

--- test/components/recommendations-list.test.jsx
+++ test/components/recommendations-list.test.jsx
@@ -7,7 +7,7 @@ import RecommendationsList from '../../src/components/recommendations-list.jsx'
 import RecommendationsResponse from '../fixtures/spotify/recommendations'
 import TrackSearchResponse from '../fixtures/spotify/track-search'
 
-const recommendation = RecommendationsResponse.tracks[0]
+const recTrack = RecommendationsResponse.tracks[0]
 
 const track = TrackSearchResponse.tracks.items[0]
 const trackSeed = {
@@ -20,22 +20,22 @@ const trackSeed = {
   albumUrl: track.album.external_urls.spotify
 }
 
+const recommendation = {
+  album: recTrack.album.name,
+  albumUrl: recTrack.album.external_urls.spotify,
+  artists: recTrack.artists.map(a => a.name),
+  id: recTrack.id,
+  image: recTrack.album.images[2].url,
+  name: recTrack.name,
+  spotifyUri: recTrack.uri,
+  url: recTrack.external_urls.spotify
+}
+
 function props(opts) {
   return {
     unauthorized: () => {},
     changeAudioFeatures: opts.changeAudioFeatures,
-    recommendations: [
-      {
-        album: recommendation.album.name,
-        albumUrl: recommendation.album.external_urls.spotify,
-        artists: recommendation.artists.map(a => a.name),
-        id: recommendation.id,
-        image: recommendation.album.images[2].url,
-        name: recommendation.name,
-        spotifyUri: recommendation.uri,
-        url: recommendation.external_urls.spotify
-      }
-    ],
+    recommendations: opts.recommendations,
     token: '123abc',
     seedType: 'track',
     seed: trackSeed
@@ -43,30 +43,54 @@ function props(opts) {
 }
 
 describe('RecommendationsList', () => {
-  let component = null
   let wantToChangeAudioFeatures = false
 
   const changeAudioFeatures = () => {
     wantToChangeAudioFeatures = true
   }
 
-  beforeEach(() => {
-    const opts = { changeAudioFeatures }
-    component = <RecommendationsList {...props(opts)} />
-  })
+  function getComponent(overrides) {
+    const opts = Object.assign({
+      changeAudioFeatures,
+      recommendations: [recommendation]
+    }, overrides || {})
+    return <RecommendationsList {...props(opts)} />
+  }
 
   afterEach(() => {
     wantToChangeAudioFeatures = false
   })
 
   test('matches snapshot', () => {
-    const tree = renderer.create(component).toJSON()
+    const tree = renderer.create(getComponent()).toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   test('can go back to change audio features', () => {
-    const button = shallow(component).find('.button.is-link')
+    const button = shallow(getComponent()).find('.button.is-link')
     button.simulate('click')
     expect(wantToChangeAudioFeatures).toBe(true)
+  })
+
+  test('does not show playlist form when playlist is built', () => {
+    const instance = shallow(getComponent()).instance()
+    instance.onPlaylistCreated({
+      name: 'whee',
+      external_urls: { spotify: '' }
+    })
+    expect(instance.playlistForm()).toBeUndefined()
+  })
+
+  test('shows how many recommendations were found', () => {
+    const title = shallow(getComponent()).find('.song-recs-title')
+    expect(title.text()).toBe('Step 3: Save playlist (1 song)')
+  })
+
+  test('pluralizes song count appropriately', () => {
+    const component = getComponent({
+      recommendations: [recommendation, recommendation]
+    })
+    const instance = shallow(component).instance()
+    expect(instance.trackCount()).toBe('2 songs')
   })
 })
